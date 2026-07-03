9460c382c165e229673781a1cdfecea324ecfc57
Kirtika Kushwah <kirtikakushwah9294@gmail.com>
2026-07-03 13:32:09 +0530

    return sum

---
+++ src/string/Program_22.java
@@ -0,0 +1,27 @@
+package string;
+//WAJP to take a String input and count all numeric characters in the String.
+import java.util.Scanner;
+public class Program_22 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter String : ");
+		String s = sc.nextLine();
+		int l = sumNumericCharacter(s);
+		System.out.println("sum : "+l);
+
+	}
+	public static int sumNumericCharacter(String s) {
+		int sum =0;
+		for(int i=0;i<s.length();i++){
+			char c = s.charAt(i);
+			if(c>=48 && c<=57) {
+				sum=sum+(c-48);
+			}
+		}
+		return sum;
+		
+	}
+	
+
+}
