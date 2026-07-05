5dd82ef57871d1c1aafb1cb0e19f13d160e176c1
Kirtika Kushwah <kirtikakushwah9294@gmail.com>
2026-07-05 12:23:12 +0530

    print more than charcater

---
+++ src/string/Program_34.java
@@ -0,0 +1,31 @@
+package string;
+//WAJP to print all the character which has appeared more than once in the String.
+
+
+import java.util.Scanner;
+public class Program_34 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter String : ");
+		String s = sc.nextLine();
+		printUniqueCharacter(s);
+		
+
+	}
+	public static void printUniqueCharacter(String s) {
+		int[] freq = new int[256];
+		for(int i=0;i<s.length();i++) {
+			freq[s.charAt(i)]++;
+		}
+		for(int i=0;i<freq.length;i++) {
+			if(freq[i]>1) {
+				System.out.println((char)(i) +" is "+ freq[i]+" times");
+			}
+		}
+		
+		
+	}
+	
+
+}
